9e2ed53ab9d5326f2f1b1b58e9af7f05ee052e32
Roberto Carrillo <roberto_car_av@hotmail.com>
2022-02-03 13:52:13 -0600

    Add balance

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,10 @@ class User < ApplicationRecord
   has_many :inverse_friendships, foreign_key: 'friend_id', class_name: 'Friendship'
   has_many :inverse_friends, through: :inverse_friendships, source: :user
 
-  
-
   # validations
   validates :email, uniqueness: true
   validates :email, :name, :last_name, presence: true
+
+  validates :balance, presence: true,
+                      numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1_000 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_161844) do
+ActiveRecord::Schema.define(version: 2022_02_03_194746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2022_02_03_161844) do
     t.string "last_name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "balance", default: 0, null: false
   end
 
   add_foreign_key "friendships", "users"
