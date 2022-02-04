ff6553e76565333ee80ea32bfa7d97f935420259
Roberto Carrillo <roberto_car_av@hotmail.com>
2022-02-03 19:27:58 -0600

    Add balance tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  namespace :user do
-      post ':id/payment', to: 'payments#create', as: :payment
-      get ':id/feed', to: 'feeds#index', as: :feed
-      get ':id/balance', to: 'balances#show', as: :balance
+  defaults format: :json do
+    namespace :user do
+        post ':id/payment', to: 'payments#create', as: :payment
+        get ':id/feed', to: 'feeds#index', as: :feed
+        get ':id/balance', to: 'balances#show', as: :balance
+    end
   end
 end
---
+++ spec/requests/user/balances_spec.rb
@@ -0,0 +1,27 @@
+RSpec.describe User::BalancesController, type: :request do
+  describe 'GET #show' do
+    let(:user) { create(:user, balance: 100) }
+
+    before { get user_balance_path(user) }
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns user balance' do
+      expect(response.body).to eq({user: { balance: 100 }}.to_json)
+    end
+
+    context 'when user does not exist' do
+      before { get user_balance_path(id: 0) }
+
+      it 'returns http not found' do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it 'returns error message' do
+        expect(response.body).to eq({error: 'Record not found'}.to_json)
+      end
+    end
+  end
+end
